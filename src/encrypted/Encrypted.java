427956b45c83eca1763c650bc1b22e9470952b25
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-07-21 10:34:53 +0200

    update4
    line argument in console

--- src/encrypted/Encrypted.java
+++ src/encrypted/Encrypted.java
@@ -7,18 +7,62 @@ public class Encrypted {
 	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		String choix = sc.nextLine();
-		String msg = sc.nextLine();
-		int key = sc.nextInt();
+		String choix = mode(args);
+		int key = key(args);
+		String msg = data(args);
+		
+		if (msg.startsWith("\""))
+			msg = msg.substring(1,msg.length() - 1);
 		
 		switch (choix) {
 		case "enc":
-			System.out.println(msgCrypt(msg, key));
+			System.out.println(msgCrypt(msg, key).trim());
 			break;	
 		case "dec":
-			System.out.println(msgDecrypt(msg, key));
+			System.out.println(msgDecrypt(msg, key).trim());
 			break;
 		}
+	}
+	
+	static String mode(String[] arr) {
+		String md = "enc";
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equals("-mode")) {
+				md = arr[++i];
+				break;
+			}
+		}
+		return md;
+	}
+	static int key(String[] arr) {
+		int key = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equals("-key")) {
+				key = Integer.parseInt(arr[++i]);
+				break;
+			}
+		}
+		return key;
+	}
+	static String data(String[] arr) {
+		String d = "";
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].equals("-data")) {
+				i++;
+				d += arr[i];
+				i++;
+				while(i < arr.length) {
+					if (!arr[i].startsWith("-")) {
+						d = d + " " + arr[i];
+						i++;
+					}
+					else
+						break;
+				}
+				break;
+			}
+		}
+		return d;
 	}
 	
 	static char encrypt(int key , char ch) {
